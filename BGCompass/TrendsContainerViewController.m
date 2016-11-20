e58b25fe2c3182457961584d33625b83c8950a51
beepscore <support@beepscore.com>
2016-11-19 17:15:20 -0800

    In TrendsContainerViewController delete unused template initWithNibName

--- BGCompass/TrendsContainerViewController.m
+++ BGCompass/TrendsContainerViewController.m
@@ -28,15 +28,6 @@
     APPaginalTableView *_paginalTableView;
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
