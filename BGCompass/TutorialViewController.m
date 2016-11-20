839d5e4dc262529c0bf0c8678cb00b62bca2a3b1
beepscore <support@beepscore.com>
2016-11-19 17:20:59 -0800

    In TutorialViewController delete unused template initWithNibName

--- BGCompass/TutorialViewController.m
+++ BGCompass/TutorialViewController.m
@@ -47,16 +47,6 @@
 
 @implementation TutorialViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-        
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
