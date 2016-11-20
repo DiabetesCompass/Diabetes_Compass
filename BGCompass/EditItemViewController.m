8df3bb9d1d57e0a7a9d5b375aa42449952c397a1
beepscore <support@beepscore.com>
2016-11-19 17:26:21 -0800

    In EditItemViewController delete unused template initWithNibName

--- BGCompass/EditItemViewController.m
+++ BGCompass/EditItemViewController.m
@@ -27,15 +27,6 @@
 
 @implementation EditItemViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 #pragma mark - view
 
 - (void)viewDidLoad
