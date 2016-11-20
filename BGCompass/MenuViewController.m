9f7f51f722d442d241bec8d9a63fdfcb454439db
beepscore <support@beepscore.com>
2016-11-19 16:34:54 -0800

    In MenuViewController delete unused template initWithNibName

--- BGCompass/MenuViewController.m
+++ BGCompass/MenuViewController.m
@@ -22,15 +22,6 @@
 
 @implementation MenuViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
