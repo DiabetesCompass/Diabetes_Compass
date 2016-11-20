f1372fdd538c598337e0df09329a816dcde72bae
beepscore <support@beepscore.com>
2016-11-19 18:03:47 -0800

    In UPCScannerViewController delete commented out template method prepareForSegue

--- BGCompass/UPCScannerViewController.m
+++ BGCompass/UPCScannerViewController.m
@@ -237,16 +237,4 @@ didOutputMetadataObjects:(NSArray *)metadataObjects
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-
 @end
