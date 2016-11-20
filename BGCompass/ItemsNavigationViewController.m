81aa5ee90d12fa94cfa5e6d89236cc5369cd0b98
beepscore <support@beepscore.com>
2016-11-19 17:23:36 -0800

    In ItemsNavigationViewController delete unused template initWithNibName

--- BGCompass/ItemsNavigationViewController.m
+++ BGCompass/ItemsNavigationViewController.m
@@ -17,16 +17,6 @@
 
 @implementation ItemsNavigationViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-        
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
