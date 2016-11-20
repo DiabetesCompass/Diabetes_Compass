268b5fb09fe51138547d2de38adba284491e21ce
beepscore <support@beepscore.com>
2016-11-19 16:16:07 -0800

    In AddBGOverlayViewController delete unused template initWithNibName

--- BGCompass/AddBGOverlayViewController.m
+++ BGCompass/AddBGOverlayViewController.m
@@ -20,14 +20,6 @@
 
 @implementation AddBGOverlayViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
