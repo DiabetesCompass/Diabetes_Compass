3003852ad422dfccfd9e98298714a2bb31662573
beepscore <support@beepscore.com>
2016-12-09 20:01:55 -0800

    Add TrendsViewController.swift and Trends.storyboard

---
+++ BGCompass/BGCompass-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+// https://developer.apple.com/library/content/documentation/Swift/Conceptual/BuildingCocoaApps/MixandMatch.html
+
+#import "GraphViewController.h"
