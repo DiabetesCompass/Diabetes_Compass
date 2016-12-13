f9bf0e78480fe22156cbc54448723d2140b60937
beepscore <support@beepscore.com>
2016-12-12 22:22:24 -0800

    Added TrendsViewControllerTests.swift It runs and test example passes

---
+++ BGCompassTests/BGCompassTests-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "Constants.h"
+#import "TrendsAlgorithmModel.h"
+#import "CorePlot-CocoaTouch.h"
+#import "GraphViewController.h"
---
+++ BGCompassTests/TrendsViewControllerTests.swift
@@ -0,0 +1,19 @@
+//
+//  TrendsViewControllerTests.swift
+//  BGCompass
+//
+//  Created by Steve Baker on 12/10/16.
+//  Copyright © 2016 Clif Alferness. All rights reserved.
+//
+
+import XCTest
+@testable import BGCompass
+
+class TrendsViewControllerTests: XCTestCase {
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+        
+}
