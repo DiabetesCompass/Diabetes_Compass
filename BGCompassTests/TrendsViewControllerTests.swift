0f31b597ae7a1fce9f5a514ba138aa3683e5e05f
beepscore <support@beepscore.com>
2016-12-13 22:10:28 -0800

    Add testBloodGlucoseTextReading, comment out body that isn’t working yet

--- BGCompassTests/TrendsViewControllerTests.swift
+++ BGCompassTests/TrendsViewControllerTests.swift
@@ -19,6 +19,21 @@ class TrendsViewControllerTests: XCTestCase {
         XCTAssertEqual(TrendsViewController.bloodGlucoseText(reading: nil), "No data")
     }
 
+    func testBloodGlucoseTextReading() {
+        // Probably easier to test this from Objective C
+        // Not sure how to do it in Swift.
+        // https://www.andrewcbancroft.com/2015/01/13/unit-testing-model-layer-core-data-swift/
+        // http://stackoverflow.com/questions/29313645/swift-coredata-unittest-how-to-avoid-exc-breakpoint
+        // http://stackoverflow.com/questions/24662780/swift-cannot-test-core-data-in-xcode-tests/26795795#26795795
+        // let context = NSManagedObjectContext.MR_newContext()
+        // let bg = BGReading.MR_createEntity()
+        // bg.name = "Blood Glucose"
+        // bg.setQuantity(72,  withConversion: false)
+        // bg.timeStamp = Date()
+        // print("testBloodGlucoseTextReading", bg)
+        // XCTAssertEqual(TrendsViewController.bloodGlucoseText(reading: bg), "72 mg/d")
+    }
+
     func testHa1cTextReadingNil() {
         XCTAssertEqual(TrendsViewController.ha1cText(reading: nil), "No data")
     }
