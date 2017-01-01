a149077974bfeb731794ed89644d11449e3f43cc
beepscore <support@beepscore.com>
2017-01-01 14:36:09 -0800

    In TrendsAlgorithmModel delete commented out code including correctTrendReadingsAfterDate

--- BGCompass/TrendsAlgorithmModel.m
+++ BGCompass/TrendsAlgorithmModel.m
@@ -109,27 +109,6 @@
     return result;
 }
 
-/*<<<<<<< HEAD
-=======
-- (void) correctTrendReadingsAfterDate:(NSDate*) lowerBound
-{
-//    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"timeStamp >= %@", lowerBound];
-    
-    NSArray *fetchedReadings = [BGReading MR_findAllSortedBy:@"timeStamp" ascending:YES inContext:[NSManagedObjectContext MR_defaultContext]];
-    NSLog(@"%lu readings", (unsigned long)fetchedReadings.count);
-    NSArray *fetchedHa1c = [Ha1cReading MR_findAllSortedBy:@"timeStamp" ascending:YES inContext:[NSManagedObjectContext MR_defaultContext]];
-    for (Ha1cReading* reading in fetchedHa1c) {
-        [reading MR_deleteEntityInContext:[NSManagedObjectContext MR_defaultContext]];
-    }
-
-    for (BGReading* reading in fetchedReadings) {
-        NSLog(@"Correcting reading for %f", MG_PER_DL_PER_MMOL_PER_L*reading.quantity.floatValue);
-        [self calculateHa1c:reading];
-    }
-}
->>>>>>> e2c94af448cb13ede477432b662a513cc6a3440b
-*/
-
 - (void) computeHA1c:(NSDate*) timeStamp {
     BGReading* lastReading = [BGReading MR_findFirstOrderedByAttribute:@"timeStamp" ascending:NO inContext:[NSManagedObjectContext MR_defaultContext]];
         int HEMOGLOBIN_LIFESPAN = 100*HOURS_IN_ONE_DAY*SECONDS_IN_ONE_HOUR;
@@ -185,35 +164,6 @@
         reading.timeStamp = lastReading.timeStamp;
         [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
         [self loadArrays];
-/*<<<<<<< HEAD
-    }
- //           NSLog(@"weighted average BG: %f", CONVERSIONFACTOR*twBGAve);
-            twHA1c = (46.7 + CONVERSIONFACTOR*twBGAve)/28.7;
-            //log &Add final result to CoreData
- //           NSLog(@"weighted average HA1c: %f", twHA1c);
-            Ha1cReading* reading = [Ha1cReading MR_createEntityInContext:[NSManagedObjectContext MR_defaultContext]];
-            reading.quantity = @(twHA1c);
-            //set the timestamp of this HA1c to the timestamp of the last BG reading?
-            reading.timeStamp = lastReading.timeStamp;
-            [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
-            [self loadArrays];
-=======
-            NSLog(@"BG indexed: %f", MG_PER_DL_PER_MMOL_PER_L*reading.quantity.floatValue);
-        previousReading = reading;
-   }
-        twBGAve = (sum)/sumRamp;
-    }
-    NSLog(@"weighted average BG: %f", MG_PER_DL_PER_MMOL_PER_L*twBGAve);
-    twHA1c = (46.7 + MG_PER_DL_PER_MMOL_PER_L*twBGAve)/28.7;
-    //log &Add final result to CoreData
-    NSLog(@"weighted average HA1c: %f", twHA1c);
-    Ha1cReading* reading = [Ha1cReading MR_createEntityInContext:[NSManagedObjectContext MR_defaultContext]];
-    reading.quantity = @(twHA1c);
-    //set the timestamp of this HA1c to the timestamp of the last BG reading?
-    reading.timeStamp = lastReading.timeStamp;
-    [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
-    [self loadArrays];
->>>>>>> e2c94af448cb13ede477432b662a513cc6a3440b*/
 }
 
 @end
