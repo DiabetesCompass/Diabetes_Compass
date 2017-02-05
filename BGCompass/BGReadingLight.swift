6ce6e57a7735f11cf5e669d1b9183c928bff4f29
beepscore <support@beepscore.com>
2017-02-05 12:46:30 -0800

    Add BGReadingLight and BGReadingLightTests

---
+++ BGCompass/BGReadingLight.swift
@@ -0,0 +1,25 @@
+//
+//  BGReadingLight.swift
+//  BGCompass
+//
+//  Created by Steve Baker on 2/5/17.
+//  Copyright © 2017 Clif Alferness. All rights reserved.
+//
+
+import UIKit
+
+/**
+A "lightweight" version of BGReading that is not a CoreData managed object.
+It may be instantiated from a BGReading, and is easy to use and test without a CoreData context.
+*/
+struct BGReadingLight {
+
+    var timeStamp: Date
+    var quantity: Float
+
+    init(bgReading: BGReading) {
+        timeStamp = bgReading.timeStamp
+        quantity = bgReading.quantity as Float
+    }
+
+}
