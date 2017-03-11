7f1671f7ef05a2b51bde73c75cd68d46b8842164
beepscore <support@beepscore.com>
2017-03-11 10:13:25 -0800

    formatting- In EditItemViewController delete commented out code and extra whitespace.

--- BGCompass/EditItemViewController.m
+++ BGCompass/EditItemViewController.m
@@ -29,8 +29,7 @@
 
 #pragma mark - view
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     [self.view setBackgroundColor:[UIColor clearColor]];
@@ -52,34 +51,14 @@
     self.titleLabel.returnKeyType = UIReturnKeyDone;
     UIColor *color = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.8];
     self.titleLabel.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Click to edit name..." attributes:@{NSForegroundColorAttributeName: color}];
-    //UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap:)];
-    //[self.view addGestureRecognizer:singleTap];
-    
-    
-//    CGRect bigSlider;
-//    CGRect smallSlider;
-//    
-//    if([[NSUserDefaults standardUserDefaults] floatForKey:SETTING_SCREEN_CONSTANT] == 1) {
-//        smallSlider = CGRectMake((320-170)/2, 160, 170, 170);
-//        bigSlider = CGRectMake((320-270)/2, 110, 270, 270);
-//    } else {
-//        smallSlider = CGRectMake((320-130)/2, 140, 130, 130);
-//        bigSlider = CGRectMake((320-220)/2, 95, 220, 220);
-//    }
-    
     
     if ([self.item isKindOfClass:[BGReading class]]) {
-        //self.valueSlider = [[EFCircularSlider alloc] initWithFrame:bigSlider];
         [self generateBGForm];
     } else if ([self.item isKindOfClass:[InsulinReading class]]) {
-        //self.valueSlider = [[EFCircularSlider alloc] initWithFrame:bigSlider];
         [self generateInsulinForm];
     } else if ([self.item isKindOfClass:[FoodReading class]]) {
-        //self.valueSlider = [[EFCircularSlider alloc] initWithFrame:smallSlider];
-        //self.carbsSlider = [[EFCircularSlider alloc] initWithFrame:bigSlider];
         [self generateFoodForm];
     }
-    
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -117,32 +96,6 @@
 
 - (void)generateBGForm {
     
-    /*self.valueSlider.minimumValue = 0.0;
-    self.valueSlider.lineWidth = 6;
-    self.valueSlider.handleColor = [UIColor colorWithRed:1.0 green:64.0/255.0 blue:67.0/255.0 alpha:0.9];
-    self.valueSlider.handleType = EFDoubleCircleWithOpenCenter;
-    self.valueSlider.unfilledColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.2];
-    self.valueSlider.filledColor = [UIColor colorWithRed:1.0 green:64.0/255.0 blue:67.0/255.0 alpha:0.9];
-    self.valueSlider.labelFont = [UIFont fontWithName:@"HelveticaNeue" size:13];
-    self.valueSlider.labelColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.5];
-    [self.valueSlider addTarget:self action:@selector(newValue:) forControlEvents:UIControlEventValueChanged];
-    
-    NSMutableArray *labelsArray = [NSMutableArray new];
-    if([[NSUserDefaults standardUserDefaults] boolForKey:SETTING_SHOULD_DISPLAY_BG_IN_MMOL_PER_L]) {
-        self.valueSlider.maximumValue = 300.0/MG_PER_DL_PER_MMOL_PER_L;
-        [self.valueSlider setCurrentValue:[((BGReading*)self.item).quantity floatValue]];
-        self.valueUnitsLabel.text = @"mmol/L";
-        for(int i=1; i<11; i++) {
-            [labelsArray addObject:[NSString stringWithFormat:@"%.0f", i*30.0/MG_PER_DL_PER_MMOL_PER_L]];
-        }
-    } else {
-        self.valueSlider.maximumValue = 300;
-        [self.valueSlider setCurrentValue:[((BGReading*)self.item).quantity floatValue]*MG_PER_DL_PER_MMOL_PER_L];
-        self.valueUnitsLabel.text = @"mg/dL";
-        for(int i=1; i<11; i++) {
-            [labelsArray addObject:[NSString stringWithFormat:@"%.0f", i*30.0]];
-        }
-    }*/
     if([[NSUserDefaults standardUserDefaults] boolForKey:SETTING_SHOULD_DISPLAY_BG_IN_MMOL_PER_L]) {
         self.valueUnitsLabel.text = @"mmol/L";
     } else {
@@ -160,57 +113,9 @@
     self.carbsUnitsLabel.hidden = YES;
     self.carbsField.hidden = YES;
     [self.valueField becomeFirstResponder];
-    
-    //[self.valueSlider setInnerMarkingLabels:labelsArray];
-    //[self.view addSubview:self.valueSlider];
 }
 
 - (void)generateFoodForm {
-    
-    
-    /*self.valueSlider.minimumValue = 0.0;
-    self.valueSlider.maximumValue = 5;
-    self.valueSlider.lineWidth = 6;
-    self.valueSlider.handleColor = [UIColor colorWithRed:1.0 green:64.0/255.0 blue:67.0/255.0 alpha:0.9];
-    self.valueSlider.handleType = EFDoubleCircleWithOpenCenter;
-    self.valueSlider.unfilledColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.2];
-    self.valueSlider.filledColor = [UIColor colorWithRed:1.0 green:64.0/255.0 blue:67.0/255.0 alpha:0.9];
-    self.valueSlider.labelFont = [UIFont fontWithName:@"HelveticaNeue" size:13];
-    self.valueSlider.labelColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.5];
-    [self.valueSlider addTarget:self action:@selector(newValue:) forControlEvents:UIControlEventValueChanged];
-
-    NSArray* labelsArray = @[@"1", @"2", @"3", @"4", @"5"];
-    
-    [self.valueSlider setInnerMarkingLabels:labelsArray];
-    
-    
-    self.carbsSlider.minimumValue = 0.0;
-    self.carbsSlider.maximumValue = 300;
-    self.carbsSlider.snapToLabels = NO;
-    self.carbsSlider.lineWidth = 8;
-    
-    self.carbsSlider.handleColor = [UIColor colorWithRed:29.0/255.0 green:207.0/255.0 blue:0.0 alpha:1.0];
-    self.carbsSlider.handleType = EFDoubleCircleWithOpenCenter;
-    self.carbsSlider.unfilledColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.2];
-    self.carbsSlider.filledColor = [UIColor colorWithRed:29.0/255.0 green:207.0/255.0 blue:0.0 alpha:1.0];
-    self.carbsSlider.labelFont = [UIFont fontWithName:@"HelveticaNeue" size:13];
-    self.carbsSlider.labelColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.5];
-    [self.carbsSlider addTarget:self action:@selector(newCarbsValue:) forControlEvents:UIControlEventValueChanged];
-
-    
-    NSMutableArray *labelsArray2 = [NSMutableArray new];
-    for(int i=1; i<11; i++) {
-        [labelsArray2 addObject:[NSString stringWithFormat:@"%i", i*30]];
-    }
-    
-    [self.carbsSlider setInnerMarkingLabels:labelsArray2];
-    [self.view addSubview:self.carbsSlider];
-    [self.view addSubview:self.valueSlider];
-    
-    [self.valueSlider setCurrentValue:[((FoodReading*)self.item).numberOfServings floatValue]];
-    [self.carbsSlider setCurrentValue:[((FoodReading*)self.item).carbs floatValue]];
-    */
-    
     self.valueField.text = [((FoodReading*)self.item).numberOfServings stringValue];
     self.valueField.text = [NSString stringWithFormat:@"%.1f", [((FoodReading*)self.item).numberOfServings floatValue]];
     self.valueTitleLabel.text = @"Servings";
@@ -227,29 +132,9 @@
     
     self.carbsField.text = [NSString stringWithFormat:@"%.0f", [((FoodReading*)self.item).carbs floatValue]];
     [self.carbsField becomeFirstResponder];
-    
 }
 
 - (void)generateInsulinForm {
-    
-    /*self.valueSlider.minimumValue = 0.0;
-    self.valueSlider.lineWidth = 6;
-    self.valueSlider.handleColor = [UIColor colorWithRed:67.0/255.0 green:64.0/255.0 blue:1 alpha:0.9];
-    self.valueSlider.handleType = EFDoubleCircleWithOpenCenter;
-    self.valueSlider.unfilledColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.2];
-    self.valueSlider.filledColor = [UIColor colorWithRed:67.0/255.0 green:64.0/255.0 blue:1 alpha:0.9];
-    self.valueSlider.labelFont = [UIFont fontWithName:@"HelveticaNeue" size:13];
-    self.valueSlider.labelColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.5];
-    [self.valueSlider addTarget:self action:@selector(newValue:) forControlEvents:UIControlEventValueChanged];
-    
-    NSMutableArray *labelsArray = [NSMutableArray new];
-    
-    self.valueSlider.maximumValue = 300.0/18.0;
-    [self.valueSlider setCurrentValue:[((InsulinReading*)self.item).quantity floatValue]];
-    for(int i=1; i<11; i++) {
-        [labelsArray addObject:[NSString stringWithFormat:@"%i", i*1]];
-    }
-    */
 
     NSArray* InsulinMappingArray = @[INSULINTYPE_STRING_REGULAR, INSULINTYPE_STRING_GLULISINE, INSULINTYPE_STRING_LISPRO, INSULINTYPE_STRING_ASPART];
 // TODO: fix me this array should be placed somewhere else. Some sort of constants place. Also located in SettingsController.m
@@ -257,14 +142,6 @@
         self.valueField.text = [NSString stringWithFormat:@"%.2f", [((InsulinReading*)self.item).quantity floatValue]];
         self.valueTitleLabel.text = [@"Insulin - " stringByAppendingString:[InsulinMappingArray objectAtIndex:((InsulinReading*)self.item).insulinType.intValue]];
     } else {
-        /* 
-        float deficit = [[[BGAlgorithmModel sharedInstance] getDeficit] floatValue];
-        // Use this code to default the insulin dose to the deficit.
-        if (deficit < 0) {
-            self.valueField.text = [NSString stringWithFormat:@"%.2f", -1*deficit];
-        } else {
-            self.valueField.text = @"0";
-        }*/
         self.valueField.text = @"0";
         self.valueTitleLabel.text = [@"Insulin - " stringByAppendingString:[InsulinMappingArray objectAtIndex:[[NSUserDefaults standardUserDefaults] integerForKey:SETTING_INSULIN_TYPE]]];
     }
@@ -274,16 +151,12 @@
     self.carbsUnitsLabel.hidden = YES;
     self.carbsField.hidden = YES;
     [self.valueField becomeFirstResponder];
-    //[self.valueSlider setInnerMarkingLabels:labelsArray];
-    //[self.view addSubview:self.valueSlider];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 - (void) newValue:(EFCircularSlider*)sender {
     if([[NSUserDefaults standardUserDefaults] boolForKey:SETTING_SHOULD_DISPLAY_BG_IN_MMOL_PER_L]) {
@@ -297,20 +170,17 @@
     self.carbsField.text = [NSString stringWithFormat:@"%.0f", sender.currentValue];
 }
 
-
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     [textField endEditing:YES];
     [self.valueField becomeFirstResponder];
     return NO;
 }
 
-
 - (void) saveFood:(FoodReading*)food asNew:(BOOL)isNew {
     food.name = self.titleLabel.text;
     food.carbs = [NSNumber numberWithFloat:[self.carbsField.text floatValue]];
     food.timeStamp = self.date;
     food.numberOfServings = [NSNumber numberWithFloat:[self.valueField.text floatValue]];
-    //food.servingUnitAndQuantity = self.servingSizeItem.value;
     if (isNew) {
         food.isPending = [NSNumber numberWithBool:YES];
     }
@@ -346,14 +216,11 @@
     [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
 }
 
-
-
 - (IBAction)doneClicked:(id)sender {
     NSLocale *locale = [NSLocale currentLocale];
     NSDateFormatter *theDate = [NSDateFormatter new];
     [theDate setLocale:locale];
     [theDate setDateFormat:@"h:mm a MMMM d, yyyy"];
-    
     
     self.doneButtonClicked = YES;
     if (self.editingMode) {
